d0ce0d673fce4c1f3cece90e61523a6b755389f4
Xu Qiuling <t-xuqiul@microsoft.com>
2017-07-14 16:01:15 +0800

    Visual Studio 2015 PROJECT INITIAITE.

---
+++ CacheService/CacheService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Transactions;
+
+namespace CacheService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
