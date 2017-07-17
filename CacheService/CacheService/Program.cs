5763c55013167282c55537fe8d23cfae3072d936
Xu Qiuling <t-xuqiul@microsoft.com>
2017-07-17 09:50:09 +0800

    test

--- CacheService/CacheService/Program.cs
+++ CacheService/CacheService/Program.cs
@@ -14,6 +14,17 @@ namespace CacheService
     {
         static void Main(string[] args)
         {
+            using (var ignite = Ignition.Start())
+            {
+                var cache = ignite.GetOrCreateCache<int, string>("myCache");
+
+                // Store keys in cache (values will end up on different cache nodes).
+                for (int i = 0; i < 10; i++)
+                    cache.Put(i, i.ToString());
+
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine("Got [key={0}, val={1}]", i, cache.Get(i));
+            }
         }
     }
 }
