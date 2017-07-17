7e73450e6c9f7c2d9736563174573f316a279311
Xu Qiuling <t-xuqiul@microsoft.com>
2017-07-17 11:29:42 +0800

    add the framework.

---
+++ Cache/CacheService/QueryFeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CacheService
+{
+    class QueryFeed
+    {
+        static void Main(string[] args)
+        {
+            // For test 
+        }
+
+        string GetNextQuery()
+        {
+            return "NotImplement";
+        }
+    }
+}
